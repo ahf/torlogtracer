87fb8a76a67ee7d3bba72b12c5cb151c8c0dfdd2
Alexander Faeroey <ahf@torproject.org>
2017-10-09 23:31:38 +0200

    End of day update.

--- src/tor_log.rs
+++ src/tor_log.rs
@@ -4,20 +4,13 @@ use std::fmt;
 
 use self::regex::{Regex, Captures};
 
-/*
-fn foo<T>(f: T) -> i32 
-  where T : Fn(String) -> i32 {
-    f(String::from("foo"))
-}
-*/
-
-struct TorLogEntryHandler<F> {
+struct TorLogEntryHandler {
     matcher: Regex,
-    fun: F,
+    fun: Box<Fn(Captures) -> TorLogEntry>,
 }
 
-impl<F> TorLogEntryHandler<F> where F : Fn(Captures) -> TorLogEntry {
-    pub fn new(r: Regex, f: F) -> TorLogEntryHandler<F> {
+impl TorLogEntryHandler {
+    pub fn new(r: Regex, f: Box<Fn(Captures) -> TorLogEntry>) -> TorLogEntryHandler {
         TorLogEntryHandler {
             matcher: r,
             fun: f,
@@ -37,25 +30,36 @@ impl<F> TorLogEntryHandler<F> where F : Fn(Captures) -> TorLogEntry {
 pub enum TorLogEntry {
     LogOpened { version: String, git_version: String },
     FoundUsableDescriptor { onion: String },
-    Unknown   { content: String },
+    DescriptorFetchRequest { onion: String, blinded_key: String, directory_id: String, directory_nickname: String, directory_ip: String },
+    Unknown { content: String },
 }
 
 impl TorLogEntry {
     pub fn from_string(s: String) -> TorLogEntry {
         let mut handlers = Vec::new();
 
-        handlers.push(TorLogEntryHandler::new(Regex::new(r"Tor (.*) \((.*)\) opening new log file.").unwrap(), |a| {
-            let version     = a.get(1).unwrap().as_str().to_string();
-            let git_version = a.get(2).unwrap().as_str().to_string();
+        handlers.push(TorLogEntryHandler::new(Regex::new(r"Tor (.*) \((.*)\) opening new log file.").unwrap(), Box::new(|c| {
+            let version     = c.get(1).unwrap().as_str().to_string();
+            let git_version = c.get(2).unwrap().as_str().to_string();
 
             TorLogEntry::LogOpened { version, git_version }
-        }));
+        })));
 
-        handlers.push(TorLogEntryHandler::new(Regex::new(r"connection_ap_handle_onion\(\): Found usable descriptor in cache for (.*)\.onion. Not fetching\.\.").unwrap(), |b| {
-            let onion = b.get(1).unwrap().as_str().to_string();
+        handlers.push(TorLogEntryHandler::new(Regex::new(r"connection_ap_handle_onion\(\): Found usable descriptor in cache for (.*). Not fetching\.\.").unwrap(), Box::new(|c| {
+            let onion = c.get(1).unwrap().as_str().to_string();
 
             TorLogEntry::FoundUsableDescriptor { onion }
-        }));
+        })));
+
+        handlers.push(TorLogEntryHandler::new(Regex::new(r"directory_launch_v3_desc_fetch\(\): Descriptor fetch request for service (.*) with blinded key (.*) to directory \$(.*)\~(.*) at (.*)").unwrap(), Box::new(|c| {
+            let onion = c.get(1).unwrap().as_str().to_string();
+            let blinded_key = c.get(2).unwrap().as_str().to_string();
+            let directory_id = c.get(3).unwrap().as_str().to_string();
+            let directory_nickname = c.get(4).unwrap().as_str().to_string();
+            let directory_ip = c.get(5).unwrap().as_str().to_string();
+
+            TorLogEntry::DescriptorFetchRequest { onion, blinded_key, directory_id, directory_nickname, directory_ip }
+        })));
 
         for handler in handlers {
             match handler.handle(&s) {
@@ -72,7 +76,11 @@ impl fmt::Display for TorLogEntry {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             &TorLogEntry::LogOpened { ref version, ref git_version } =>
-                write!(f, "LogOpened({}, {})", version, git_version),
+                write!(f, "LogOpened(version: {}, git: {})", version, git_version),
+            &TorLogEntry::FoundUsableDescriptor { ref onion } =>
+                write!(f, "FoundUsableDescriptor(onion: {}.onion)", onion),
+            &TorLogEntry::DescriptorFetchRequest { ref onion, ref blinded_key, ref directory_id, ref directory_nickname, ref directory_ip } =>
+                write!(f, "DescriptorFetchRequest(onion: {}.onion, blinded_key: {}, directory {{ ID: {}, nickname: {}, IP: {} }}", onion, blinded_key, directory_id, directory_nickname, directory_ip),
             &TorLogEntry::Unknown { ref content } =>
                 write!(f, "Unknown({})", content),
         }
